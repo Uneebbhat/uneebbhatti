16a5522dbbc0df4c2e9148220ac84e232cab83a6
Uneeb <uneebbhatti3@gmail.com>
2023-08-07 16:33:18 +0500

    new social link added

--- src/Components/Coffee/Coffee.js
+++ src/Components/Coffee/Coffee.js
@@ -5,7 +5,10 @@ export default function coffee() {
   return (
     <div className="coffee-wrapper">
       <a href="https://www.buymeacoffee.com/uneebbhatti">
-        <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=uneebbhatti&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff" />
+        <img
+          src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=uneebbhatti&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff"
+          alt="By me a coffee"
+        />
       </a>
     </div>
   );
