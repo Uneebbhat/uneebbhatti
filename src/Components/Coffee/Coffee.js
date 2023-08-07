abee7cfc9dd9dc319a5af682bcd32aa6b516f10d
Uneeb <uneebbhatti3@gmail.com>
2023-08-07 16:13:01 +0500

    buy me a coffee button added

--- src/Components/Coffee/Coffee.js
+++ src/Components/Coffee/Coffee.js
@@ -4,11 +4,8 @@ import "./Coffee.css";
 export default function coffee() {
   return (
     <div className="coffee-wrapper">
-      <a href="https://www.buymeacoffee.com/uneebbhatti" target="_blank">
-        <img
-          src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=uneebbhatti&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff"
-          alt="By me a coffee"
-        />
+      <a href="https://www.buymeacoffee.com/uneebbhatti">
+        <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=uneebbhatti&button_colour=FFDD00&font_colour=000000&font_family=Cookie&outline_colour=000000&coffee_colour=ffffff" />
       </a>
     </div>
   );
