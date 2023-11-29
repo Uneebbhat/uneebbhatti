79a7f26ce91588866937e54c7593374ca1caf2b1
Uneeb <uneebbhatti3@gmail.com>
2023-11-30 00:41:23 +0500

    updating banner design updated

--- js/app.js
+++ js/app.js
@@ -2,12 +2,3 @@ function toggleHide() {
   const mobNav = document.querySelector(".mob-nav");
   mobNav.classList.toggle("nav-active");
 }
-
-window.onload = function () {
-  setTimeout(function () {
-    var underDevMessage = document.getElementById("underDevMessage");
-    if (underDevMessage) {
-      underDevMessage.style.display = "none";
-    }
-  }, 3000);
-};
