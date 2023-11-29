5c4343f3dbd1bea4d8c4229e3f6b1eb8366a153a
Uneeb <uneebbhatti3@gmail.com>
2023-11-29 23:41:23 +0500

    under development message added

--- js/app.js
+++ js/app.js
@@ -2,3 +2,12 @@ function toggleHide() {
   const mobNav = document.querySelector(".mob-nav");
   mobNav.classList.toggle("nav-active");
 }
+
+window.onload = function () {
+  setTimeout(function () {
+    var underDevMessage = document.getElementById("underDevMessage");
+    if (underDevMessage) {
+      underDevMessage.style.display = "none";
+    }
+  }, 3000);
+};
