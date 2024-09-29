e471194e6d4b71a2f32afdb2b9adfccd0e5b7893
Uneebbhat <uneebbhatti3@gmail.com>
2024-09-29 20:48:50 +0500

    button added to projects

--- src/pages/Projects/ProjectsPage.tsx
+++ src/pages/Projects/ProjectsPage.tsx
@@ -1,10 +1,18 @@
 import MainLayout from "@/layout/MainLayout";
 import PageLayout from "@/layout/PageLayout";
 import { FC } from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import clientflow from "@/assets/clientflow.svg";
 import coinbounce from "@/assets/coinbounce.jpg";
 import tokentalent from "@/assets/tokentalent.png";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 const ProjectsPage: FC = () => {
   const porjects = [
@@ -13,18 +21,21 @@ const ProjectsPage: FC = () => {
       logo: clientflow,
       description:
         "ClientFlow is an all-in-one SaaS platform that enables businesses, freelancers, and agencies to manage clients, collaborate with teams, and track projects through integrated tools like chat, Kanban boards, and cloud storage.",
+      link: "https://www.upwork.com/freelancers/uneeb?p=1817639829192945664",
     },
     {
       name: "Coinbounce",
       logo: coinbounce,
       description:
         "Coinbounce is a dynamic blogging platform for cryptocurrency enthusiasts to share insights and market analysis, featuring an intuitive interface for creating posts and integrating real-time crypto data for enhanced engagement.",
+      link: "https://www.upwork.com/freelancers/uneeb?p=1809312830301208576",
     },
     {
       name: "TokenTalentHR",
       logo: tokentalent,
       description:
         "TokentalentHR is a mobile app that simplifies employee attendance and leave management, allowing users to mark attendance, apply for leaves, and view payrolls with a user-friendly interface and real-time updates.",
+      link: "https://www.upwork.com/freelancers/uneeb?p=1840411648092487680",
     },
   ];
   return (
@@ -45,6 +56,11 @@ const ProjectsPage: FC = () => {
                 <CardContent>
                   <p>{project.description}</p>
                 </CardContent>
+                <CardFooter>
+                  <Link to={project.link} target="_blank">
+                    <Button variant={"secondary"}>View on upwork</Button>
+                  </Link>
+                </CardFooter>
               </Card>
             ))}
           </div>
