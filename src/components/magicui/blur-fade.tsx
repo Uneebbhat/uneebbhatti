4ff317125991690d937f9accc832820f97fa0380
Uneeb Bhatti <uneebbhatti3@gmail.com>
2025-06-23 00:39:57 +0500

    type error resolved

--- src/components/magicui/blur-fade.tsx
+++ src/components/magicui/blur-fade.tsx
@@ -14,7 +14,7 @@ interface BlurFadeProps {
   delay?: number;
   yOffset?: number;
   inView?: boolean;
-  inViewMargin?: string;
+  inViewMargin?: string | number | unknown | any;
   blur?: string;
 }
 const BlurFade = ({
