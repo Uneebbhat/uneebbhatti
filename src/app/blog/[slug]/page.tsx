c3b2bbf85df1989f34937f3033ef7c975d74f7c1
Uneeb Bhatti <uneebbhatti3@gmail.com>
2025-07-02 17:29:55 +0500

    Blog page updated

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -65,7 +65,7 @@ export default async function Blog({
   }
 
   return (
-    <section id="blog">
+    <section id="blog" className="max-w-2xl mx-auto px-4">
       <script
         type="application/ld+json"
         suppressHydrationWarning
