87c3f812d09f14959b0c8f1bd12c54b09a66a1ca
Uneeb Bhatti <uneebbhatti3@gmail.com>
2025-07-02 20:56:56 +0500

    vercel error resolved

--- src/app/api/admin/get-blog/route.ts
+++ src/app/api/admin/get-blog/route.ts
@@ -38,37 +38,3 @@ export async function GET(req: NextRequest) {
 export function POST() {
   return NextResponse.json({ error: "Method Not Allowed" }, { status: 405 });
 }
-
-export function formatDate(date: string) {
-  if (typeof date !== "string" || !date) {
-    return "Invalid date";
-  }
-  let currentDate = new Date().getTime();
-  if (!date.includes("T")) {
-    date = `${date}T00:00:00`;
-  }
-  let targetDate = new Date(date).getTime();
-  let timeDifference = Math.abs(currentDate - targetDate);
-  let daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
-
-  let fullDate = new Date(date).toLocaleString("en-us", {
-    month: "long",
-    day: "numeric",
-    year: "numeric",
-  });
-
-  if (daysAgo < 1) {
-    return "Today";
-  } else if (daysAgo < 7) {
-    return `${fullDate} (${daysAgo}d ago)`;
-  } else if (daysAgo < 30) {
-    const weeksAgo = Math.floor(daysAgo / 7);
-    return `${fullDate} (${weeksAgo}w ago)`;
-  } else if (daysAgo < 365) {
-    const monthsAgo = Math.floor(daysAgo / 30);
-    return `${fullDate} (${monthsAgo}mo ago)`;
-  } else {
-    const yearsAgo = Math.floor(daysAgo / 365);
-    return `${fullDate} (${yearsAgo}y ago)`;
-  }
-}
